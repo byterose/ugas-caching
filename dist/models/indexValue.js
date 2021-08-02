bd43138a2c668d7644abcebac37602e56abc6c57
byterose <>
2021-08-03 00:23:26 +0200

    Remove dist

--- dist/models/indexValue.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const indexSchema = new mongoose.Schema({
-    cycle: { type: String, require: false },
-    timestamp: { type: Date, require: true },
-    price: { type: String, require: true }
-});
-module.exports = mongoose.model('Index', indexSchema);
-//# sourceMappingURL=indexValue.js.map
